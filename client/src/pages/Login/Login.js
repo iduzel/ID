5c2e92529e3e45468edf4f3cba32720fdc3fb0ae
iduzel <iduzel49@gmail.com>
2022-10-19 15:13:44 +0200

    basic setup-1

---
+++ client/src/pages/Login/Login.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ValidationTextFields from '../../components/LoginForm/LoginForm';
+import './Login.scss';
+
+const Login = () => {
+  return (
+   
+      <ValidationTextFields />
+   
+  )
+}
+
+export default Login
